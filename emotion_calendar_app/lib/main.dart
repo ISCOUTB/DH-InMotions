4591e5f14cc908baaad78f7ffdabaf907eebbab5
Lala <lauratorres2129@gmail.com>
2024-11-13 13:44:29 -0500

    Unificacion de mains

--- emotion_calendar_app/lib/main.dart
+++ emotion_calendar_app/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+// Importaciones de las versiones web
 import 'register_page.dart';
 import 'emotion_list_page.dart';
 import 'login_page.dart';
 import 'emotion_calendar_page.dart';
-import 'biblioteca_archivos.dart'; // Importa la clase BibliotecaArchivos
+import 'biblioteca_archivos.dart';
+
+// Importaciones de las versiones móvil
+import 'RegisterMovil.dart';
+import 'EmotionListMovil.dart';
+import 'LoginMovil.dart';
+import 'CalendarPageMovil.dart';
+import 'BibliotecaMovil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +36,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: ResponsiveHomePage(),
+    );
+  }
+}
+
+class ResponsiveHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Obtenemos el ancho de la pantalla
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    // Definimos un límite para considerar "móvil" (por ejemplo, 600 píxeles)
+    const mobileWidthThreshold = 600;
+
+    if (screenWidth < mobileWidthThreshold) {
+      // Si es menor al límite, muestra el diseño móvil
+      return MobileApp();
+    } else {
+      // Si es mayor o igual al límite, muestra el diseño web
+      return WebApp();
+    }
+  }
+}
+
+// Aplicación web
+class WebApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       initialRoute: '/login',
       routes: {
         '/register': (context) => RegisterPage(),
@@ -35,8 +73,24 @@ class MyApp extends StatelessWidget {
             EmotionListPage(userEmail: 'user@example.com'),
         '/calendar': (context) =>
             EmotionCalendarPage(userEmail: 'testuser@example.com'),
-        '/instructions': (context) =>
-            BibliotecaArchivos(), // Cambia la ruta a BibliotecaArchivos
+        '/instructions': (context) => BibliotecaArchivos(),
+      },
+    );
+  }
+}
+
+// Aplicación móvil
+class MobileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginMovil(),
+        '/register': (context) => RegisterMovil(),
+        '/calendar': (context) => CalendarPageMovil(userEmail: ''),
+        '/biblioteca': (context) => BibliotecaMovil(),
+        '/emotionList': (context) => EmotionListMovil(userEmail: ''),
       },
     );
   }
